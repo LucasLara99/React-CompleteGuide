fe54d5bf8c030c78f754e06b0208da66acc1d620
Lucas Lara <lucaslara99@gmail.com>
2024-08-29 12:33:39 +0200

    Starting project completed

---
+++ Starting Project/src/App.jsx
@@ -0,0 +1,18 @@
+import Examples from './components/Examples/Examples.jsx';
+import Header from './components/Header/Header.jsx';
+import CoreConcepts from './components/CoreConcepts/CoreConcepts.jsx';
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <CoreConcepts />
+        <Examples />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ Starting Project/src/components/CoreConcept/CoreConcept.jsx
@@ -0,0 +1,10 @@
+//Usamos destructuring
+export default function CoreConcept({ image, description, title }) {
+   return (
+      <li>
+         <img src={image} alt={description} />
+         <h3>{title}</h3>
+         <p>{description}</p>
+      </li>
+   );
+}
