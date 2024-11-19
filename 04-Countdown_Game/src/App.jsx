705966bff369cb81088f9cbd10a6720aa20c5336
Lucas Lara <lucaslara99@gmail.com>
2024-11-19 10:42:02 +0100

    feat: Add TimerChallenge component and integrate it into the App

--- 04-Countdown_Game/src/App.jsx
+++ 04-Countdown_Game/src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Fácil" targetTime={1} />
+        <TimerChallenge title="Medio" targetTime={5} />
+        <TimerChallenge title="Difícil" targetTime={10} />
+        <TimerChallenge title="Experto" targetTime={15} />
+      </div>
     </>
   );
 }
